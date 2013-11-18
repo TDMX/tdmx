dfde61f610dc32529e87888319ade091c73fc531
Peter Klauser <pjklauser@gmail.com>
2013-11-18 21:07:13 +0000

    added the tdmx apis.

---
+++ serviceprovider-api/src/main/java/org/tdmx/client/serviceprovider/common/DateAdapter.java
@@ -0,0 +1,31 @@
+package smt.foundation.xml.jaxb.adapter;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * Binds java.util.Calendar to a xsd:date by invoking the JAXB provider's
+ * implementation of parse and print methods.
+ * 
+ * @see global jaxb bindings in schema
+ */
+public class DateAdapter extends XmlAdapter<String, Calendar>{
+
+    public Calendar unmarshal(String value) {
+        return (DatatypeConverter.parseDate(value));
+    }
+
+    public String marshal(Calendar value) {
+        if (value == null) {
+            return null;
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        formatter.setTimeZone(value.getTimeZone());
+        
+        return formatter.format(value.getTime());
+    }
+
+}
