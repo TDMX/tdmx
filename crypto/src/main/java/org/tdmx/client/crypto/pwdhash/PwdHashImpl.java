3a608faa4c3122b8d11867f7875be43c7f2ea2c9
Peter Klauser <pjklauser@gmail.com>
2013-11-19 21:45:36 +0000

    use the propper entropy source.

--- crypto/src/main/java/org/tdmx/client/crypto/pwdhash/PwdHashImpl.java
+++ crypto/src/main/java/org/tdmx/client/crypto/pwdhash/PwdHashImpl.java
@@ -1,12 +1,14 @@
 package org.tdmx.client.crypto.pwdhash;
 
+import org.tdmx.client.crypto.entropy.EntropySource;
+
 /**
  * A more secure alternative to MD5 hashing, based on jBCrypt.
  */
 public class PwdHashImpl implements PwdHash {
 
 	public static String hashpw(String password) {
-		return BCrypt_v03.hashpw(password,  BCrypt_v03.gensalt());
+		return BCrypt_v03.hashpw(password,  BCrypt_v03.gensalt(12, EntropySource.getSecureRandom()));
 	}
 	
 	public static boolean checkpw(String plaintext, String hashed) {
